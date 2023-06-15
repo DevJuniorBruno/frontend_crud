825f57392b2bc7dab6739d9a444fe81d05b2d430
DevJuniorBruno <brunode2001@gmail.com>
2023-06-15 10:23:10 -0300

    complete responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import api from '../services/api';
 import { useEffect, useState, FormEvent, useContext } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
 import Head from "next/head";
+import styles from '../../styles/home.module.scss'
 
 interface Data {
   id:string;
@@ -10,9 +11,6 @@ interface Data {
 }
 
 export default function Dashboard(){
-
-  
-
   const { createCurso, deleteCurso, editCurso, setEditCursoId} = useContext(AuthContext)
 
   const [name, setName] = useState("")
@@ -21,119 +19,75 @@ export default function Dashboard(){
   const[loading, setLoading] = useState(true);
   
   useEffect(()=>{
-
     async function loadApi(){
-      const response = await api.get("/cursos")
-      console.log(response.data)
+      const response = await api.get("/cursos");
       
       setLoading(false)
       setData(response.data)
     }
-    
     loadApi()
-    
-
   },[])
-
   async function handleAddCurso(event: FormEvent<HTMLFormElement>){
     event.preventDefault();
     let dataSubmit = {
       name    
     }
-
     if(itemId){
       await editCurso({name, id:itemId})
-
-
     }else{
       await createCurso(dataSubmit)
     }
-
-    
   }
-
   async function handleRmCurso(id:string){
-    
-
     await deleteCurso(id)
-
     console.log(id)
-
   }
-
   async function handleEditCurso(id:string, name:string){
-
     setEditCursoId(id)
     setName(name)
     setItemId(id)
-
-
     console.log(id, name)
   }
-
-
     if(loading){
-
       return(
-
-        
-        <div>
+        <div className={styles.container} >
            <Head>
             <title>TODO LIST</title>
           </Head>
           <h2>carregando filmes</h2>
         </div>
       )
-
     }
-      return(
-
-        
-        <div>
-
+      return( 
+        <div className={styles.container} >
           <Head>
-            <title>TODO LIST</title>
+            <title>TO DO LIST</title>
           </Head>
-
-          <h2>Home Page</h2>
-
-          <form onSubmit={handleAddCurso} >
-            <input 
-              placeholder="Digite seu curso"
-              value={name}
-              
-              onChange={(e)=> setName(e.target.value)}
-
-              >
-            </input>
-
-            <button>{itemId ? "Atualizar" : "Add"}</button>
-          </form>
-  
+          <div className={styles.containerSection} >
+          <div className={styles.todoList} >
+            <h2 className={styles.h2} >Tarefas</h2>
+              <form className={styles.form} onSubmit={handleAddCurso} >
+                <input 
+                  placeholder="Escreva sua tarefa"
+                  value={name}
+                  onChange={(e)=> setName(e.target.value)}
+                  >
+                </input>
+                <button>{itemId ? "Atualizar" : "Add"}</button>
+              </form>
+          </div>
             {data.map(item=>{
               return(
-
-                <article key={item.id}>
-
+                <article className={styles.list}  key={item.id}>
                 <strong>{item.name} </strong>
-
+                <div>
                 <button onClick={()=> handleEditCurso(item.id, item.name)} >Edit</button>
-
-
                 <button onClick={()=> handleRmCurso(item.id)}>Delete</button>
-
+                </div>
                 </article>
-
               )
           })}
-
-
-       
-
-    
-
-
-      <Link href='/about'> Link About </Link>
+          </div>    
     </div>
   )
     }
